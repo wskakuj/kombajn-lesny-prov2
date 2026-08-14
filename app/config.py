292a36ec61297d19f84577926e919603c028e8b1
wskakuj <nie.pyszcz@gmail.com>
2026-08-14 12:49:24 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -18,7 +18,7 @@ from pathlib import Path
 import customtkinter as ctk
 
 # --- WERSJA I AKTUALIZACJA ---
-CURRENT_VERSION = "v1.5.2"
+CURRENT_VERSION = "v1.5.3"
 GITHUB_USER = "wskakuj"
 GITHUB_REPO = "kombajn-lesny-prov2"
 
